90b8d3165ea4975c9b2606cca3ee6a3f2ea57813
vinicius costa <vitonilcius@gmail.com>
2023-03-08 17:05:56 -0300

    Assunto: feat: Adicionar array com os dados a buscar
    - Foi inserido o arquivo script.js e o arquivo hino.js - No arquivo hino.js está um módolo com as letras das músicas - No arquivo script.js está o array com os dados - Foi feito um import e export do modulo hinos para ser usado no array hinos

---
+++ hinos.js
@@ -0,0 +1,190 @@
+const l1 = `Santo, santo, santo! Deus onipotente! 
+            Cedo de manhã cantaremos Teu louvor
+            Santo, santo, santo! Deus Jeová triúno!
+            És Deus excelso nosso Criador
+
+            Santo, santo, santo! Nós, os pecadores
+            Não podemos ver Tua glória sem tremor
+            Tu somente és santo justo e compassivo
+            Puro e perfeito nosso Redentor
+
+            Santo, santo, santo! Deus onipotente!
+            Tua criação manifesta o Teu amor
+            Antes de criares todo o Céu e a Terra
+            Eras e sempre hás de ser, Senhor`;   
+
+const l2 = `Ó adorai o Senhor na beleza
+            Da santidade da glória e poder
+            Vinde e prostrai-vos à Sua grandeza
+            Ele é o princípio e sempre há de ser
+
+            Vinde a Seus pés Ele é caridade
+            Grande e piedoso tardio em furor
+            Vinde adorá-Lo por Sua bondade
+            Pela grandeza do Seu santo amor
+
+            Ele criou o Universo e a Terra
+            Vinde e adorai-O com fé e oração
+            Límpida fonte de bênçãos encerra
+            Fonte de graça e de redenção`;    
+
+const l3 = `O Deus eterno reina
+            No monte de Sião
+            Chegai-vos ante o trono seu
+            Em santa adoração
+            Trazei humildemente
+            O vosso coração
+            Ao Deus eterno e Rei dos reis
+            Senhor da redenção
+
+            O Deus eterno reina
+            No monte de Sião
+            Os astros na amplidão sem fim
+            Sustenta com a mão
+            A Ele toda a honra
+            A glória e o poder!
+            E ascendam os louvores mil
+            Ao mais sublime ser
+
+            O Deus eterno reina
+            No monte de Sião
+            Entrai no santo templo seu
+            Buscai o seu perdão
+            Buscando sua face
+            A paz encontrareis
+            E reine para sempre, enfim
+            O eterno Rei dos reis`;  
+
+const l4 = `Ó anjos e homens dai a Deus louvores
+            Pois Sua graça dá aos pecadores!
+            Ó firmamento Céus e mar e Terra
+            Sim, dai louvor a Deus!
+
+            Vós, ó remidos, dai louvor a Cristo!
+            Tal sacrifício, oh jamais foi visto!
+            A Sua morte Seu amor revela
+            Sim, adorai Jesus!
+
+            Quão insondável é o amor divino!
+            O Santo Espírito ilumina o mundo
+            É tão sublime luz ao peregrino
+            Luz que ao Céu conduz`;    
+
+const l5 = `Jubilosos te adoramos
+            Deus da glória, Deus do amor
+            Nosso coração transborda
+            Em louvor a Ti, Senhor
+            Rompe as nuvens da tristeza
+            Do pecado e do temor
+            Doador da eterna graça
+            Enche-nos do Teu amor!
+
+            Tuas obras anunciam
+            Teu poder e resplendor
+            Céus e terra cantam hinos
+            Em louvor a ti, Senhor
+            Vales, montes e campinas
+            Lindos prados, verde mar
+            Aves, fontes cristalinas
+            Todos querem te adorar!
+
+            Aleluia, aleluia
+            A Jesus, o redentor!
+            Aleluia eternamente
+            Em louvor a Ti, Senhor!
+            Ao consolador dai glória
+            Pois conforto e vida traz
+            Aleluia ao pai eterno
+            Deus da graça, Deus da paz!`;   
+
+const l6 = `Nós Te adoramos Deus infinito
+            E a Teu Filho Rei, Salvador
+            Teu nome eterno é sempre bendito
+            Teu Espírito traz-nos amor
+            Glória rendemos honra, louvor
+            E adoração a Ti, ó Senhor!
+
+            Éramos presos pelo pecado
+            Tu nos livraste dessa prisão
+            E, por Teu Filho imaculado
+            Ofertaste-nos a salvação
+            Glória rendemos honra, louvor
+            E adoração a Ti, Redentor!
+
+            Tu nos convences dos vis pecados
+            E da justiça luzes nos dás
+            E à verdade somos guiados
+            Ó Espírito Santo de paz
+            Glória rendemos honra, louvor
+            E adoração ó Consolador!`;
+
+const l7 = `Sejas louvado
+            Rei, criador da natura!
+            Louva, minh'alma
+            Louva com zelo e ternura!
+            Louva o senhor
+            Pelo insondável amor
+            Cálido amor que perdura!
+
+            Sejas louvado
+            Tu que o universo conduzes!
+            Sob tuas asas
+            Paz e conforto produzes
+            Haja louvor
+            Pelo teu dom redentor
+            Príncipe eterno das luzes!
+
+            Sejas louvado
+            Pelo cuidado paterno!
+            Sobre a verdade
+            Ergues teu reino eterno
+            Teu o louvor
+            Seja somente, senhor
+            Por Teu amor sempiterno!`;
+
+const l8 = `Ó Deus de amor vimos nós Te adorar
+            Vós, ó nações rendei louvor!
+            És Tu, Senhor o poderoso Vencedor
+            És Criador e Rei sem-par
+
+            Por Teu poder nos criaste, Senhor
+            Deste-nos vida e perfeição
+            Mas, ao deixarmos o Teu plano de amor
+            Veio em Jesus a salvação
+
+            Teu é o poder e o louvor, nosso Deus
+            Teu, nosso amor fervor também
+            Qual rocha firme a verdade ficará
+            Deus eternal Senhor. Amém!`;
+
+const l9 = `Santo, santo Pai bondoso
+            Santo, santo Deus poderoso
+            Entregamos nosso coração a Ti, em devoção
+            Pai bondoso Te adoramos
+
+            Santo, santo Cristo amado
+            Pela cruz nós somos redimidos
+            Elevamos nossa mente em constante adoração
+            Cristo amado Te adoramos
+
+            Santo, santo Santo Espírito
+            Enche de poder a nossa vida
+            Suplicamos Tua graça Teu auxílio e direção
+            Santo Espírito Te adoramos`;
+
+const l10 = `Santo, santo Pai bondoso
+            Santo, santo Deus poderoso
+            Entregamos nosso coração a Ti, em devoção
+            Pai bondoso Te adoramos
+
+            Santo, santo Cristo amado
+            Pela cruz nós somos redimidos
+            Elevamos nossa mente em constante adoração
+            Cristo amado Te adoramos
+
+            Santo, santo Santo Espírito
+            Enche de poder a nossa vida
+            Suplicamos Tua graça Teu auxílio e direção
+            Santo Espírito Te adoramos`;
+
+export {l1, l2, l3, l4, l5, l6, l7, l8, l9, l10};
---
+++ script.js
@@ -0,0 +1,19 @@
+// Armazenamento das informações //
+
+import {l1, l2, l3, l4, l5, l6, l7, l8, l9, l10} from './hinos.js'
+
+const hinos = [
+    { numero: 1, nome: "Santo, Santo, Santo"       , letra:  l1},
+    { numero: 2, nome: "Ó Adorai o Senhor"         , letra:  l2},
+    { numero: 3, nome: "O Deus Eterno Reina"       , letra:  l3},
+    { numero: 4, nome: "Louvor ao Trino Deus"      , letra:  l4},
+    { numero: 5, nome: "Jubilosos Te Adoramos"     , letra:  l5},
+    { numero: 6, nome: "Nós Te Adoramos"           , letra:  l6},
+    { numero: 7, nome: "Sejas Louvado"             , letra:  l7},
+    { numero: 8, nome: "Ó Deus de Amor"            , letra:  l8},
+    { numero: 9, nome: "Santo, Santo, Pai Bondoso" , letra:  l9},
+    { numero: 10, nome: "O Senhor Está Aqui"       , letra:  l10}
+];
+
+//Função de busca de dados//
+
